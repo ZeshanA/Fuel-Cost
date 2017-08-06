01562ce7cd9651457431e34e79b16b78495af2a2
Zeshan Amjad <zeshan@zesh.me>
2017-08-06 13:18:37 +0100

    Add user input for units

--- calc.py
+++ calc.py
@@ -1,9 +1,32 @@
-def calculateCost(mpg, distance, cost_per_gal):
-    gals_used = distance / mpg
-    return cost_per_gal * gals_used
+def calculateCost(efficiency, distance, cost_per_unit):
+    units_used = distance / efficiency
+    return cost_per_unit * units_used
 
-mpg = int(input("What MPG did you attain?\n"))
-distance = int(input("How far did you travel?\n"))
-cost_per_gal = int(input("In pence, how much does 1 gallon of fuel cost?\n"))
+unit_names = {"M": "miles", "K": "kilometres", "G": "gallons", "L": "litres"}
 
-print(calculateCost(mpg, distance, cost_per_gal))
+# Unit Questions
+distance_unit = input("Enter M for miles or K for kilometres:\n").capitalize()
+
+efficiency_volume_unit = input("If you use " + unit_names[distance_unit]
+                               + " per gallon, enter G. If you use "
+                               + unit_names[distance_unit]
+                               + " per litre, enter L.\n").capitalize()
+
+cost_volume_unit = input("If you buy fuel in gallons, enter G, if you buy fuel"
+                         + " in litres enter L.\n").capitalize()
+
+cost_unit = input("What is your currency?\n")
+
+# Calculation Questions
+efficiency = int(input("How many " + unit_names[distance_unit]
+                       + " per "+ unit_names[efficiency_volume_unit]
+                       + " did you attain?\n"))
+
+distance = int(input("How many " + unit_names[distance_unit]
+                     + " did you travel?\n"))
+
+cost_per_unit = float(input("In " + cost_unit
+                      + ", how much does 1 " + unit_names[cost_volume_unit]
+                      + " of fuel cost?\n"))
+
+print(calculateCost(efficiency, distance, cost_per_unit))
