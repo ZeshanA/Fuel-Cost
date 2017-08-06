6d248ca89f970c5669b09f0e0a2b134e6f1f209a
Zeshan Amjad <zeshan@zesh.me>
2017-08-06 20:17:25 +0100

    Remove unnecessary import

--- fetch.py
+++ fetch.py
@@ -1,6 +1,5 @@
 from lxml import html
 import requests
-import units
 
 
 def fetch_price():
@@ -9,5 +8,4 @@ def fetch_price():
     tree = html.fromstring(page.content)
     price = float(
         tree.xpath('//*[@id="fuel-price-pump"]/div/div[1]/span/text()')[0]) / 100
-
     return price
